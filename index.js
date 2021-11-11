1633bc5fb7893b5fbfa984b10aa90a34d7cb5867
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-12 00:25:02 +0600

    get and post user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const cors = require('cors');
 require('dotenv').config();
@@ -18,12 +19,87 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('connected to database');
-        // const database = client.db('streetPottery');
-        // const servicesCollection = database.collection('products');
+        // console.log('connected to database');
+        const database = client.db('streetPottery');
+        const productsCollection = database.collection('products');
+        const usersCollection = database.collection('users');
+
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // GET Single Product
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific product', id);
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.json(product);
+        })
+
+        // GET user
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
 
 
+        // POST API
+        app.post('/products', async (req, res) => {
 
+            // create a document to insert
+            const product = req.body;
+            console.log('hit the post api', product);
+            const result = await productsCollection.insertOne(product);
+            console.log(result);
+            res.json(result)
+            // res.send('post hitted');
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            // console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            // const requester = req.decodedEmail;
+            // if (requester) {
+            //     const requesterAccount = await usersCollection.findOne({ email: requester });
+            //     if (requesterAccount.role === 'admin') {
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+            // }
+            // }
+            // else {
+            //     res.status(403).json({ message: 'you do not have access to make admin' })
+            // }
+
+        })
 
     }
     finally {
@@ -36,7 +112,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('street pottery server')
 })
 
 app.listen(port, () => {
