f4fcd4fe09e98cc7253f1f079340e49e26b4060d
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-12 11:38:06 +0600

    delete product api done

--- index.js
+++ index.js
@@ -90,6 +90,14 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
+
+        // DELETE API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
